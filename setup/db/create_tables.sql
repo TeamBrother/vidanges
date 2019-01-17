e34d672ddac88a7eaef5b481fbceab0a5b241543
chinjto <cyril_defaye@yahoo.fr>
2019-01-17 22:16:52 +0100

    Ajout d'entretien et de voiture dans create_db.sql

--- setup/db/create_tables.sql
+++ setup/db/create_tables.sql
@@ -4,3 +4,18 @@ CREATE TABLE `vidange`.`proprietaire` (
   `prenom` VARCHAR(45) NULL,
   `adresse` VARCHAR(45) NULL,
   PRIMARY KEY (`id`));
+
+CREATE TABLE `vidange`.`entretien` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `date` DATETIME NULL,
+  `idvoiture` INT(11) NULL,
+  PRIMARY KEY (`id`));
+
+CREATE TABLE `vidange`.`proprietaire` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `couleur` VARCHAR(10) NULL,
+  `marque` VARCHAR(45) NULL,
+  `modele` VARCHAR(45) NULL,
+  `proprietaire` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
+  
