8dd802cb3b58f81096dbda183e5713c5854f3384
chinjto <cyril_defaye@yahoo.fr>
2019-01-17 21:49:12 +0100

    Création du suivi SQL

---
+++ setup/db/create_tables.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `vidange`.`proprietaire` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `nom` VARCHAR(45) NULL,
+  `prenom` VARCHAR(45) NULL,
+  `adresse` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
